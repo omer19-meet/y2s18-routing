f3a8f0917dc64bf63abb1617ba97baa7a304b82a
omer19-meet <omer19@meet.mit.edu>
2018-08-08 07:12:16 -0400

    not finish

--- exercises/app.py
+++ exercises/app.py
@@ -4,6 +4,24 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'START HERE: CHANGE ME'
+    return render_template("home.html")
+
+
+@app.route('/student/<int:student_id>')
+def student_id(student_id):
+	return render_template("student.html",student_id = student_id ) 
+
+add_student("Alex","2019", False )
+
+
+def display_student(student_id):
+
+	return render_template("student.html",
+		# student_id =  student_id,
+		student= query_by_id(student_id)
+		)
+
+display_student(1)
+
 
 app.run(debug=True)
